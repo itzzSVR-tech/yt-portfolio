35e544be704ff3b9e195e749890fd2a5982da958
itzzSVR-tech <marioplayz1975@gmail.com>
2025-06-04 11:47:24 +0530

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,5 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
+    base: "/yt-portfolio",
 });
